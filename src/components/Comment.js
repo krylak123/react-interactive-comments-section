5b430b557bbb6d1beab1192b90eb8eee82c40c28
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-01-18 21:33:05 +0100

    created HTML structure for Comment and Reply

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,6 +5,12 @@ import { AppContext } from '../context/GlobalStore';
 
 import Reply from './Reply';
 
+import IconPlus from '../images/icon-plus.svg';
+import IconMinus from '../images/icon-minus.svg';
+import IconReply from '../images/icon-reply.svg';
+import IconEdit from '../images/icon-edit.svg';
+import IconDelete from '../images/icon-delete.svg';
+
 const Comment = ({ id, user, createdAt, score, content, replies }) => {
   const { currentUser } = useContext(AppContext);
 
@@ -13,7 +19,44 @@ const Comment = ({ id, user, createdAt, score, content, replies }) => {
   return (
     <>
       <li className="comment">
-        <p>comment</p>
+        <div className="comment__title-container">
+          <p className="comment__name">{user.username}</p>
+          {user.username === currentUser.username ? (
+            <p className="comment__signature">you</p>
+          ) : null}
+          <p className="comment__time">{createdAt}</p>
+        </div>
+        <div className="comment__content-container">
+          <p className="comment__content">{content}</p>
+        </div>
+        <div className="comment__score-container">
+          <button type="button" className="comment__score-btn">
+            <img src={IconPlus} alt="" className="comment__score-img" />
+          </button>
+          <p className="comment__score-value">{score}</p>
+          <button type="button" className="comment__score-btn">
+            <img src={IconMinus} alt="" className="comment__score-img" />
+          </button>
+        </div>
+        <div className="comment__btn-container">
+          {user.username === currentUser.username ? (
+            <>
+              <button type="button" className="comment__btn comment__btn--reply">
+                <img src={IconDelete} alt="" className="comment__btn-img" />
+                Delete
+              </button>
+              <button type="button" className="comment__btn comment__btn--reply">
+                <img src={IconEdit} alt="" className="comment__btn-img" />
+                Edit
+              </button>
+            </>
+          ) : (
+            <button type="button" className="comment__btn comment__btn--reply">
+              <img src={IconReply} alt="" className="comment__btn-img" />
+              Reply
+            </button>
+          )}
+        </div>
       </li>
       {repliesMap}
     </>
--- src/components/Reply.js
+++ src/components/Reply.js
@@ -3,12 +3,57 @@ import PropTypes from 'prop-types';
 
 import { AppContext } from '../context/GlobalStore';
 
+import IconPlus from '../images/icon-plus.svg';
+import IconMinus from '../images/icon-minus.svg';
+import IconReply from '../images/icon-reply.svg';
+import IconEdit from '../images/icon-edit.svg';
+import IconDelete from '../images/icon-delete.svg';
+
 const Reply = ({ parentID, id, user, createdAt, score, content, replyingTo }) => {
   const { currentUser } = useContext(AppContext);
 
   return (
     <li className="comment comment--reply">
-      <p>reply</p>
+      <div className="comment__title-container">
+        <p className="comment__name">{user.username}</p>
+        {user.username === currentUser.username ? <p className="comment__signature">you</p> : null}
+        <p className="comment__time">{createdAt}</p>
+      </div>
+      <div className="comment__content-container">
+        <p className="comment__content">
+          {' '}
+          <span className="comment__content-user">{`@${replyingTo} `}</span>
+          {content}
+        </p>
+      </div>
+      <div className="comment__score-container">
+        <button type="button" className="comment__score-btn">
+          <img src={IconPlus} alt="" className="comment__score-img" />
+        </button>
+        <p className="comment__score-value">{score}</p>
+        <button type="button" className="comment__score-btn">
+          <img src={IconMinus} alt="" className="comment__score-img" />
+        </button>
+      </div>
+      <div className="comment__btn-container">
+        {user.username === currentUser.username ? (
+          <>
+            <button type="button" className="comment__btn comment__btn--delete">
+              <img src={IconDelete} alt="" className="comment__btn-img" />
+              Delete
+            </button>
+            <button type="button" className="comment__btn comment__btn--edit">
+              <img src={IconEdit} alt="" className="comment__btn-img" />
+              Edit
+            </button>
+          </>
+        ) : (
+          <button type="button" className="comment__btn comment__btn--reply">
+            <img src={IconReply} alt="" className="comment__btn-img" />
+            Reply
+          </button>
+        )}
+      </div>
     </li>
   );
 };

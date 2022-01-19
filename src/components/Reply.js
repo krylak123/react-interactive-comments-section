80461fe8d9fc009f4883cd749da43b6c4eb82957
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-01-19 10:33:29 +0100

    added styles for Comment and Reply

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 
 import { AppContext } from '../context/GlobalStore';
 
-import IconPlus from '../images/icon-plus.svg';
-import IconMinus from '../images/icon-minus.svg';
 import IconReply from '../images/icon-reply.svg';
 import IconEdit from '../images/icon-edit.svg';
 import IconDelete from '../images/icon-delete.svg';
@@ -28,28 +26,48 @@ const Reply = ({ parentID, id, user, createdAt, score, content, replyingTo }) =>
       </div>
       <div className="comment__score-container">
         <button type="button" className="comment__score-btn">
-          <img src={IconPlus} alt="" className="comment__score-img" />
+          <svg
+            className="comment__score-img"
+            width="11"
+            height="11"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+              fill="#C5C6EF"
+            />
+          </svg>
         </button>
         <p className="comment__score-value">{score}</p>
         <button type="button" className="comment__score-btn">
-          <img src={IconMinus} alt="" className="comment__score-img" />
+          <svg
+            className="comment__score-img"
+            width="11"
+            height="3"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+              fill="#C5C6EF"
+            />
+          </svg>
         </button>
       </div>
       <div className="comment__btn-container">
         {user.username === currentUser.username ? (
           <>
             <button type="button" className="comment__btn comment__btn--delete">
-              <img src={IconDelete} alt="" className="comment__btn-img" />
+              <img src={IconDelete} alt="" className="comment__btn-icon" />
               Delete
             </button>
             <button type="button" className="comment__btn comment__btn--edit">
-              <img src={IconEdit} alt="" className="comment__btn-img" />
+              <img src={IconEdit} alt="" className="comment__btn-icon" />
               Edit
             </button>
           </>
         ) : (
           <button type="button" className="comment__btn comment__btn--reply">
-            <img src={IconReply} alt="" className="comment__btn-img" />
+            <img src={IconReply} alt="" className="comment__btn-icon" />
             Reply
           </button>
         )}
